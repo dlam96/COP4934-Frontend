49d587243e77f387fd642a4b03b1e63561beadd7
Daniel Lam <dlam96@knights.ucf.edu>
2020-09-07 21:38:25 -0400

    toolbar styles

--- src/Components/CustomToolbar/CustomToolbar.js
+++ src/Components/CustomToolbar/CustomToolbar.js
@@ -5,21 +5,45 @@ import {
   IconButton,
   Grid,
   Button,
+  makeStyles,
 } from "@material-ui/core";
 import { ChevronLeft, ChevronRight } from "@material-ui/icons";
-
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    color: "white",
+    backgroundColor: theme.palette.primary.main,
+  },
+  icons: {
+    color: theme.palette.secondary.main,
+  },
+  disabledButton: {
+    color: "white",
+    // fontWeight: "bold",
+    // backgroundColor: theme.palette.secondary.main,
+    "&:disabled": {
+      color: theme.palette.secondary.main,
+    },
+  },
+}));
 export default function CustomToolbar(props) {
-  console.log(props);
+  // console.log(props);
+  const classes = useStyles();
+
   return (
-    <Toolbar>
+    <Toolbar className={classes.toolbar}>
       <Grid container direction="row">
         <Grid item xs={4} container justify="flex-start">
           <IconButton onClick={() => props.onNavigate("PREV")}>
-            <ChevronLeft />
+            <ChevronLeft className={classes.icons} />
           </IconButton>
-          <Button onClick={() => props.onNavigate("TODAY")}>TODAY</Button>
+          <Button
+            onClick={() => props.onNavigate("TODAY")}
+            style={{ color: "white" }}
+          >
+            TODAY
+          </Button>
           <IconButton onClick={() => props.onNavigate("NEXT")}>
-            <ChevronRight />
+            <ChevronRight className={classes.icons} />
           </IconButton>
         </Grid>
         <Grid item container xs={4} justify="center" alignItems="center">
@@ -31,26 +55,31 @@ export default function CustomToolbar(props) {
           <Button
             onClick={() => props.onView("month")}
             disabled={props.view === "month" ? true : false}
+            // classes={{ disabled: classes.disabledButton }}
+            className={classes.disabledButton}
           >
             MONTH
           </Button>
           <Button
             onClick={() => props.onView("week")}
             disabled={props.view === "week" ? true : false}
+            className={classes.disabledButton}
           >
             WEEK
           </Button>
           <Button
             onClick={() => props.onView("day")}
             disabled={props.view === "day" ? true : false}
+            className={classes.disabledButton}
           >
             DAY
           </Button>
           <Button
             onClick={() => props.onView("agenda")}
             disabled={props.view === "agenda" ? true : false}
+            className={classes.disabledButton}
           >
-            agenda
+            AGENDA
           </Button>
         </Grid>
       </Grid>
