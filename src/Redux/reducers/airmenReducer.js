9c2f19b56985d3ef28338da392829393681eb7f0
Chengalang <Kennycheng0705@gmail.com>
2020-11-29 19:22:57 -0500

    Update airmenReducer.js

--- src/Redux/reducers/airmenReducer.js
+++ src/Redux/reducers/airmenReducer.js
@@ -1,13 +1,58 @@
-import { SETAIRMEN } from '../actionTypes.js';
+import { SETAIRMEN, EDITAIRMAN, APPROVEAIRMAN, SETPENDING } from "../actionTypes.js";
 
 export default function (state = null, action) {
   switch (action.type) {
     case SETAIRMEN: {
-      return action.payload.airmen;
+      return {
+        users: [...action.payload.airmen],
+        pending: state ? state.pending ? Array.from(state.pending) : [] : []
+      }
+    }
+
+    case SETPENDING: {
+      return {
+        users: Array.from(state.users),
+        pending: [...action.payload.pending]
+      }
+    }
+
+    case EDITAIRMAN: {
+      console.log("editting got obj:", action.payload);
+      let newState = {};
+      newState.users = Array.from(state.users);
+      newState.pending = Array.from(state.pending);
+      for (let i = 0; i < newState.users.length; i++) {
+        if (newState.users[i].account_uuid === action.payload.airman.account_uuid) {
+          console.log("found user to edit");
+          newState.users[i] = Object.assign(newState.users[i], action.payload.airman);
+          break;
+        }
+      }
+      return newState;
+    }
+
+    case APPROVEAIRMAN: {
+      console.log("approving got obj:", action.payload);
+      let newState = {};
+      newState.users = Array.from(state.users);
+      newState.pending = Array.from(state.pending);
+
+      if (!action.payload.airman.airmen.length) {
+        console.log("Should not reach this spot");
+        newState.users.push(action.payload.airman.airmen);
+      } else {
+        newState.users.concat(action.payload.airman.airmen);
+        let removePendingUsers = [];
+        for (const airman of action.payload.airman.airmen) {
+          removePendingUsers.push(airman.account_uuid);
+        }
+        newState.pending = newState.pending.filter((item) => !removePendingUsers.includes(item.account_uuid));
+      }
+      return newState;
     }
 
     default: {
-      return state
+      return state;
     }
   }
 }
